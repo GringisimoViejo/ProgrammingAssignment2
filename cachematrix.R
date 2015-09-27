8759291108f01508124bdc207d2d2a965675b1a2
GringisimoViejo <robert.dunnigan@alumni.insead.edu>
2015-09-27 13:35:33 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are to be used with the following format as based on items in the discussion 
+##   board. The first function will convert vectors with 4, 9, or other square number of elements
+##   into a square matrix. Note that inverted matrices may feature scientific notation.
+##
+##   * a <- <square matrix or eligible vector for conversion to matrix> 
+##   * b <- makeCacheMatrix(a)
+##   * cacheSolve(b)
+##
+##
+## makeCacheMatrix has two functions:
+##
+##   * makeCacheMatrix$make converts displays the matrix, converting it if need be back into a square
+##       matrix
+##   * makeCacheMatrix$invert inverts the entered matrix
+ 
+makeCacheMatrix <- function(x = matrix()) { 
+    c <- NULL
+    d <- NULL
+    make <- function() {
+        z <- sqrt(length(x))
+        c <<- matrix((x), nrow = z, ncol = z)
+    }
+    invert <- function() {
+        z <- sqrt(length(x))
+        c <<- matrix((x), nrow = z, ncol = z)
+        d <<- solve(c)
+    }
+    list(make = make, invert = invert)
+} 
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
+## This seeks out displays the inverse of the matrix, with the if statement seeking the cached
+##   value from the makeCacheMatrix$invert function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    d <- x$invert()
+    if(!is.null(d)) {
+        message("getting cached data")
+        return(d)
+    }
+    c <- x$make()
+    d <- solve(c)
+    return(d)
 }
